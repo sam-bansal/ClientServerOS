ad721a2ece29c970b4c628dec4fcc472b6b770e5
sam-bansal <82222078+sam-bansal@users.noreply.github.com>
2023-04-20 08:28:35 +0530

    correct client

--- try10/client.c
+++ try10/client.c
@@ -7,285 +7,212 @@
 #include <math.h>
 #include <limits.h>
 #include <pthread.h>
+  
 #define SHM_SIZE 1024
+  
+struct request {
  
-#define TABLE_SIZE 100
+  int type;
+  int operand1;
+  int operand2;
+  char operator;
  
-struct node
-{
-  char *key;
-  int value;
-  struct node *next;
+ 
 };
- 
-struct hash_table
-{
-  struct node **buckets;
-};
- 
-// hash function
-int hash(char *key)
-{
-  int hash_value = 0;
-  for (int i = 0; i < strlen(key); i++)
-  {
-    hash_value += key[i];
-  }
-  return hash_value % TABLE_SIZE;
-}
- 
-// create a new node
-struct node *create_node(char *key, int value)
-{
-  struct node *new_node = malloc(sizeof(struct node));
-  new_node->key = strdup(key);
-  new_node->value = value;
-  new_node->next = NULL;
-  return new_node;
-}
- 
-// create a new hash table
-struct hash_table *create_hash_table()
-{
-  struct hash_table *new_table = malloc(sizeof(struct hash_table));
-  new_table->buckets = calloc(TABLE_SIZE, sizeof(struct node *));
-  return new_table;
-}
- 
-// insert a key-value pair into the hash table
-void hash_table_insert(struct hash_table *ht, char *key, int value)
-{
-  int index = hash(key);
-  struct node *head = ht->buckets[index];
-  struct node *curr = head;
-  while (curr != NULL)
-  {
-    if (strcmp(curr->key, key) == 0)
-    {
-      curr->value = value;
-      return;
-    }
-    curr = curr->next;
-  }
-  struct node *new_node = create_node(key, value);
-  new_node->next = head;
-  ht->buckets[index] = new_node;
-}
- 
-// get the value associated with a key in the hash table
-int hash_table_get(struct hash_table *ht, char *key)
-{
-  int index = hash(key);
-  struct node *curr = ht->buckets[index];
-  while (curr != NULL)
-  {
-    if (strcmp(curr->key, key) == 0)
-    {
-      return curr->value;
-    }
-    curr = curr->next;
-  }
-  return -1; // key not found
-}
- 
-// delete a key-value pair from the hash table
-void hash_table_delete(struct hash_table *ht, char *key)
-{
-  int index = hash(key);
-  struct node *head = ht->buckets[index];
-  if (head == NULL)
-  {
-    return; // key not found
-  }
-  if (strcmp(head->key, key) == 0)
-  {
-    ht->buckets[index] = head->next;
-    free(head->key);
-    free(head);
-    return;
-  }
-  struct node *prev = head;
-  struct node *curr = head->next;
-  while (curr != NULL)
-  {
-    if (strcmp(curr->key, key) == 0)
-    {
-      prev->next = curr->next;
-      free(curr->key);
-      free(curr);
-      return;
-    }
-    prev = curr;
-    curr = curr->next;
-  }
-}
- 
-char *register_failed = "Registration Unsuccesful";
- 
-int i = 0;
- 
 struct init
 {
   pthread_mutex_t mutex;
   char request[50];
   int response;
   int op;
- 
- 
 };
  
-struct request {
-	int type;
-	int operand1;
-	int operand2;
-	char operator;
- 
- 
-};
+  
 struct response {
   int result;
 };
- 
- 
-int is_prime(int n) {
-	if (n <= 1)
-		return 0;
- 
-	for (int i = 2; i <= sqrt(n); i++) {
-		if (n % i == 0)
-			return 0;
-	}
-	return 1;
-}
- 
- 
- 
-int main() {
-	struct hash_table *ht = create_hash_table();
-  printf("On the Server side\n");
+  
+int main(int c, char*argv[]) {
   int shm_id;
   key_t shm_key;
   shm_key = ftok("server.c", 0);
-  shm_id = shmget(shm_key, SHM_SIZE, IPC_CREAT | 0666);
+  shm_id = shmget(shm_key, SHM_SIZE, 0666);
   if (shm_id < 0)
   {
     perror("shmget");
     exit(1);
   }
-  char name[50];
   struct init *shm;
   // char *shmptr, *temp;
   // shmptr = shmat(shm_id, NULL, 0);
-  shm = shmat(shm_id, NULL, 0);
-  shm->op=-1;
-  strcpy(shm->request, "*");
-  // shm->response = -1;
   if (shm == (struct init *)-1)
   {
     perror("shmat");
     exit(1);
   }
+  char username[50];
+  strcpy(username, argv[1]);
+  // printf("Enter a username: ");
+  // scanf("%s", &username);
+  shm = (struct init *)shmat(shm_id, NULL, 0);
   pthread_mutexattr_t attr;
   pthread_mutexattr_init(&attr);
   pthread_mutexattr_setpshared(&attr, PTHREAD_PROCESS_SHARED);
   pthread_mutex_init(&shm->mutex, &attr);
   pthread_mutexattr_destroy(&attr);
- 
-	// Communication Channel
-	
-    
- 
-	while(1)
-	{
-    printf("Reached 1\n");
-	while(shm->op==-1)
-	{
-	//printf("Inside while\n");
+  int client_id = 0;
+  while (1)
+  {
     pthread_mutex_lock(&shm->mutex);
-    printf("Inside while %s\n", shm->request);
+    printf("Processing your request %s\n", shm->request);
     if (strcmp(shm->request, "*") == 0)
     {
-      printf("check0\n");
+      printf("The username is being sent\n");
+      strcpy(shm->request, username);
       pthread_mutex_unlock(&shm->mutex);
-      sleep(10);
-      continue;
+      break;
+    }
+    else if (shm->request == "!")
+    {
+      break;
     }
     else
     {
-      printf("check 1\n");
-      strncpy(name, shm->request, 50);
-      printf("The username received was : %s\n", name);
+      pthread_mutex_unlock(&shm->mutex);
+      continue;
+    }
+  }
+  printf("Hello\n");
+  while (1)
+  {
+    if (strcmp(shm->request, "!") == 0)
+    {
+      strcpy(shm->request, "*");
+      printf("The client id is: %d\n", shm->response);
+      client_id = shm->response;
+      pthread_mutex_unlock(&shm->mutex);
+      break;
+    }
+    else
+    {
+      continue;
+    }
+  }
  
-      if (hash_table_get(ht, name) != -1)
+  
+  
+  
+    int option;
+   
+     struct request *shm_req2;
+  struct response *shm_res;
+ 
+  int comm_id,comm_id2;
+  key_t comm_key = ftok("client.c", 0);
+  key_t commk2=ftok("client.c", client_id);
+  comm_id = shmget(comm_key, SHM_SIZE, 0666 | IPC_CREAT);
+  comm_id2 = shmget(commk2, SHM_SIZE, 0666 | IPC_CREAT);
+ 
+  printf("%d %d\n",comm_id2,comm_id);
+   printf("Reached y %d\n",comm_id);
+  if (comm_id < 0)
+  {
+    perror("shmget communication channel");
+    exit(1);
+  }
+  
+  shm_req2 = (struct request *)shmat(comm_id2, NULL, 0);
+   printf("Reached x %p %p\n",shm,shm_req2);
+  if (shm_req2 == (struct request *)-1)
+  {
+    perror("shmat communication channel");
+    exit(1);
+  }
+  
+  shm_res = (struct response *)(shm_req2 + 1);
+    while (1)
+  {
+    printf("Choose an option: \n1.Send a Request\n2.Unregister\n");
+    scanf("%d", &option);
+    
+  printf("Reached 3 %p\n",shm_res);
+  
+    if (option == 1)
+    {
+      shm->op=client_id;
+      printf("Choose One: \n1.Arithmetic\n2.Even/Odd\n3.isPrime\n4.isNegative\n");
+      int request_type;
+      scanf("%d", &request_type);
+       int client_id = 0;
+       
+      if (request_type == 1)
       {
-        printf("The user already exists\n");
-        shm->response = hash_table_get(ht, name);
+        printf("Enter first operand: \n");
+        scanf("%d", &shm_req2->operand1);
+        printf("Enter second operand: \n");
+        scanf("%d", &shm_req2->operand2);
+        printf("Enter operator (+, -, *, /): \n");
+        scanf(" %c", &shm_req2->operator);
+        shm_req2->type = 1;
+        
+        getchar();
+      }
+      else if (request_type == 2)
+      {
+        printf("Enter a number to check if it's even or odd: ");
+        scanf("%d", &shm_req2->operand1);
+        shm_req2->type = 2;
+        shm_req2->operand2 = 0;
+        shm_req2->operator= ' ';
+        getchar();
+        printf("Reached \n");
+      }
+      else if (request_type == 3)
+      {
+        printf("Enter a number to check if it's prime: ");
+        scanf("%d", &shm_req2->operand1);
+        shm_req2->type = 3;
+        shm_req2->operand2 = 0;
+        shm_req2->operator= ' ';
+        getchar();
+      }
+      else if (request_type == 4)
+      {
+        printf("This functionality is not available yet.\n");
+        continue;
       }
       else
       {
-        printf("The user was created\n");
-        shm->response = i++;
-        hash_table_insert(ht, name, shm->response);
+        printf("Please enter a valid input\n");
       }
-      strcpy(shm->request, "!");
-      pthread_mutex_unlock(&shm->mutex);
-      sleep(10);
-      continue;
-    }
-	if(shm->op !=-1)
-	break;
-	printf("Reached 4\n");
-    printf("The username of the current client is\n");
-	printf("The username of the current client is: %s\n", name);
-	strcpy(shm->request,"!");
-	//printf("%d \n",shm_req->type);
-	printf("The username was captured\n");
-	}
+      printf("Reached 2\n");
+      while (shm_req2->type != 0)
+      {
+        sleep(1);
+      }
+      if (shm_res->result == INT_MAX)
+      {
+        printf("Divide by Zero Error\n");
+      }
+      else
+      {
+        printf("The operands are  %d %d\n", shm_req2->operand1,shm_req2->operand2);
+    
+        printf("The result is %d\n", shm_res->result);
+        
+      }
+      printf("REACHED \n");
       
-	   
-	struct request *shm_req;
-	struct response *shm_res;
-	int comm_id;
-	key_t comm_key = ftok("client.c", shm->op);
-   
-	comm_id = shmget(comm_key, SHM_SIZE, 0666 | IPC_CREAT);
- 
-	 printf("Reached y %d\n",comm_id);
-	if (comm_id < 0) {
-		perror("shmget communication channel");
-		exit(1);
-	}
-    printf("Reached 2\n");
- 
-	shm_req = (struct request *) shmat(comm_id, NULL, 0);
-	 printf("Reached x %p\n",shm_req);
-	if (shm_req == (struct request *) - 1) {
-		perror("shmat communication channel");
-		exit(1);
-	}
-	shm_res = (struct response *)(shm_req + 1);
-    printf("Reached 3 %p\n",shm_res);
-    printf(" SHARED MEMORY USED IS %p\n",shm_req);
-	  while(shm_req->type==0)
-    {
-       sleep(1);
     }
-	
- 
-		
-			
-        printf("Reached 4\n");
- 
-		pthread_t thread;
-       // printf("The username is: %s\n", shm_req->username);
-	   printf("%d %d\n", shm_req->operand1,shm_req->operand2);
-	   shm_res->result= shm_req->operand1+shm_req->operand2;
-	
-	    shm_req->type=0;
-		 printf("Result: %d\n", shm_res->result);
-		    shm->op =-1;
-	
-    
-	}
-    
+    else if (option == 2)
+    {
+      // Unregister Code
+    }
+    else
+    {
+      // Error in choosing option
+      printf("Please Enter a Valid option");
+    }
+  
+  }
 }
